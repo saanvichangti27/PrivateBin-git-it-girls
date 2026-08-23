32ce24f7f3aa48275dec760836e16d8de5c1a9fe
saanvi <saanvichangti27@gmail.com>
2026-08-24 00:28:34 +0530

    added secuirty features SRI & CSP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sri from 'vite-plugin-sri'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sri()],
 })
